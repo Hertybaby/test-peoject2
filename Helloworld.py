62921ffe13330507fd82af2a2d1687abcc4c379a
Hertybaby <Hertywoolley@gmail.com>
2023-07-20 05:17:11 -0400

    display name three times

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,12 @@
-print("Helloworld")
+# print("Helloworld")
+
+# name = input ("Enter your name")
+# for items in range(0 , 3):
+#     print(name)
+
+
+name = input ("Enter your name")
+number = int (input("Enter a number"))
+for items in range(0 , number):
+    print(name)
+
