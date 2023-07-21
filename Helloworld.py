34eacf4f1f461f9984c4701be98ab752a77ace7c
Hertybaby <Hertywoolley@gmail.com>
2023-07-21 09:38:32 -0400

    For loop work

--- Helloworld.py
+++ Helloworld.py
@@ -1,16 +1,53 @@
-# print("Helloworld")
-
+## for Loops 
+   
+ ##035
 # name = input ("Enter your name")
 # for items in range(0 , 3):
 #     print(name)
 
-
+    #036
 # name = input ("Enter your name")
 # number = int (input("Enter a number"))
 # for items in range(0 , number):
 #     print(name)
+   
+ ##037
 
-user_name = input("Enter your name:  ")
+# user_name = input("Enter your name:  ")
+# for each_letter in user_name:
+#     print(each_letter)
 
-for each_letter in user_name:
-    print(each_letter)
+##038
+
+# name = input("Enter a name:  ")
+# number = int(input("Enter a number:  "))
+# for counter in range(0 ,number):
+#     for each_letter in name:
+#         print(each_letter)
+    
+   
+     ##039
+
+# user_number = int (input("Enter a number b/n 1 and 12:  "))
+# for counter in range(1, 13):
+#     result = user_number * counter
+#     print(user_number ,"*", counter, "=", result)
+
+    ##040
+
+# number = int(input("Enter a number below 50:  "))
+# for counter in range(0 , 51):
+#     print(counter)
+
+##41
+
+name = input ("Enter your name:  ")
+number = int(input("Enter a number:  "))
+for counter in range(0, number <10):
+    print(name)
+else:
+ for counter in range(1 ,4):
+    print("Too high")   
+
+    
+    
